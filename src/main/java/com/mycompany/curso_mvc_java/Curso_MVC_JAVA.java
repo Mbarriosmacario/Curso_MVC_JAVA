7ac64a38189f3e057fe6b4117ac58f01ab4177d1
Mbarriosmacario <114964259+Mbarriosmacario@users.noreply.github.com>
2022-10-10 21:33:42 -0600

    carga inicial

---
+++ src/main/java/com/mycompany/curso_mvc_java/Curso_MVC_JAVA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.curso_mvc_java;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Curso_MVC_JAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
